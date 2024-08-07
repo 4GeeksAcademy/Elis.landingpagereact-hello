34a691ae8eae8229332298d27c8887631f31f3a1
ElisG24 <160186837+ElisG24@users.noreply.github.com>
2024-08-07 16:46:47 +0000

    ejercicio completado

---
+++ src/js/component/Jumbotron.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Jumbotron = () => {
+    return (
+        <div className="text-center">
+            <div className="mt-4 p-5 bg-primary text-white rounded">
+                <h1>A Warm Welcome!</h1>
+                <p>Lorem ipsum...</p>
+            </div>
+        </div>
+    );
+};
+
+
+export default Jumbotron;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,51 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// //include images into your bundle
+// import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+// //create your first component
+
+import Card from "./Card";
+import Navbar from "./Navbar";
+import Jumbotron from "./Jumbotron";
+import Footer from "./Footer";
+
 const Home = () => {
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
+			{/* <h1 className="text-center mt-5">Hello Rigo!</h1> */}
 			<p>
-				<img src={rigoImage} />
+				{/* <img src={rigoImage} /> */}
 			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
+			<a href="#" className="">
+				{/* If you see this green button... bootstrap is working... */}
 			</a>
 			<p>
-				Made by{" "}
+				{/* Made by{" "}
 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
+				love! */}
 			</p>
+			<Navbar/>
+			<div className="Container">
+				<Jumbotron/>
+				<div className="d-sm-flex">
+					<Card title={"proyecto React"}
+					description={"Aprendiendo"}
+					buttonURL={"www.google.com"} label={"Ir a Google"} />
+					<Card title={"Ejecutando"}
+					description={"El proceso"}
+					buttonURL={"www.facebook.com"} label={"Ir a Google"} />
+					<Card title={"explorando"}
+					description={"nuevos temas"}
+					buttonURL={"www.instagram.com"} label={"Ir a Google"} />
+					<Card title={"resultado final"}
+					description={"listo pa pintura"}
+					buttonURL={"www.bootstrap.com"}label={"Ir a Google"} />
+
+				</div>
+					<Footer/>
+			</div>
 		</div>
 	);
 };
